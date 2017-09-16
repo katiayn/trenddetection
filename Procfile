dd13acdc2bfb86e66e8ec42d808df6362f51d2c8
Kátia Nakamura <katia@kiwi.com>
2017-09-16 12:00:14 +0200

    Import project

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn trenddetection.wsgi --log-file -
---
+++ trenddetection/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from rest_framework import routers
+
+from trenddetection.core import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
